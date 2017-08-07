62fb0c8b4080aee0faa5106e028ede690275e249
amirtha1408 <30686949+amirtha1408@users.noreply.github.com>
2017-08-07 16:29:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,11 @@ var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
+
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counte.toString();
+};
